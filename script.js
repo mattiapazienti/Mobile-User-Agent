8af8ddc627ad9b33cae71459f14520767f8ada70
Mattia Pazienti <kuttebo@msn.com>
2014-01-02 12:58:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var target = document.getElementsByTagName('body');
 var check = {any:function(){return navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);}};
 if(check.any())target.setAttribute('class','mobile');
+// check only iphone
+// if(check.any('iPhone'))target.setAttribute('class','iphone');
